2cbaa5b551a1cba1656dc1a5cb2732a6fa603e4e
Robert Snyder <r.snyder49@gmail.com>
2018-12-24 20:52:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    const thisYear = new Date(year).getFullYear();
+    return thisYear - this.startDate.getFullYear() + 1;
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  blocksTravelled() {
+ 		let avenues = [
+ 		  '1st Avenue',
+ 		  '2nd Avenue',
+ 		  '3rd Avenue',
+ 		  'Lexington Avenue',
+ 		  'Park',
+ 		  'Madison Avenue',
+ 		  '5th Avenue'
+ 	 ];
+ 		
+ 	 const northSouth =  Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+ 	 const eastWest = Math.abs(avenues.indexOf(this.beginningLocation.horizontal) - avenues.indexOf(this.endingLocation.horizontal));
+ 	 return northSouth + eastWest;
+  }
+  
+  estimatedTime(peak) {
+ 		if (peak === true) {
+ 			return this.blocksTravelled() / 2;
+ 		}
+ 		else {
+ 			return this.blocksTravelled() / 3;
+ 		}
+ 	}
+}
+
+
+
+
 
